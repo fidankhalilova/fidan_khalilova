dc52b561105d8b64cda5361771d8646d07da2127
fidankhalilova <fidankhalilova@Fidans-MacBook-Pro.local>
2026-07-18 00:03:27 +0400

    fix: projects

--- fidankhalilova/api/projects/index.ts
+++ fidankhalilova/api/projects/index.ts
@@ -1,18 +1,24 @@
 import { ApiResponse, ProjectType } from "@/api/projects/types";
 
 export async function fetchProjects(): Promise<ProjectType[]> {
-  const res = await fetch("http://localhost:3001/api/projects", {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/projects`,
+    {
+      cache: "no-store",
+    },
+  );
   if (!res.ok) throw new Error("Failed to fetch");
   const payload: ApiResponse<ProjectType[]> = await res.json();
   return payload.data;
 }
 
 export async function fetchProjectById(id: string): Promise<ProjectType> {
-  const res = await fetch(`http://localhost:3001/api/projects/${id}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/projects`,
+    {
+      cache: "no-store",
+    },
+  );
   if (!res.ok) {
     throw new Error(`Failed to fetch project with ID: ${id}`);
   }
